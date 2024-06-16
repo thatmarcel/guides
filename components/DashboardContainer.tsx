7ce031517beacf18150fad016028713df4bb3e03
thatmarcel <contact@thatmarcel.com>
2024-06-17 00:14:45 +0200

    Replace bottom bar with top left user button

--- components/DashboardContainer.tsx
+++ components/DashboardContainer.tsx
@@ -1,7 +1,7 @@
 import {ReactNode} from "react";
 import MiscButton from "./MiscButton";
 import BackIcon from "../icons/BackIcon";
-import DashboardBottomBar from "./DashboardBottomBar";
+import {UserButton} from "@clerk/nextjs";
 
 export default function DashboardContainer({
     children,
@@ -15,31 +15,37 @@ export default function DashboardContainer({
     shouldHaveBackButton: boolean
 }) {
     return (
-        <div className="w-screen h-full flex px-8 py-6 md:pt-20 pb-20">
-            <div className={`mx-auto max-w-[1000px] w-full ${shouldHaveBackButton ? "md:-mt-9" : ""}`}>
-                {shouldHaveBackButton
-                    ? <a href="/dash">
-                        <MiscButton isGhost={true} className="-ml-6 -mb-1">
-                            <BackIcon
-                                className="inline mr-1 mb-[2.25px] w-5 h-5 fill-black dark:fill-white"
-                            />
-
-                            Back to overview
-                        </MiscButton>
-                    </a>
-                    : null
-                }
-                <div className="flex items-center mb-8">
-                    <h1 className="text-4xl font-bold grow">
-                        {headerTitle}
-                    </h1>
-
-                    {headerButton}
+        <div className="w-screen h-full">
+            <div className="w-full pr-8 pt-6 h-16">
+                <div className="inline-block float-right">
+                    <UserButton />
                 </div>
+            </div>
 
-                {children}
+            <div className="w-full flex px-8 py-6 pt-4 pb-20">
+                <div className={`mx-auto max-w-[1000px] w-full ${shouldHaveBackButton ? "md:-mt-9" : ""}`}>
+                    {shouldHaveBackButton
+                        ? <a href="/dash">
+                            <MiscButton isGhost={true} className="-ml-6 -mb-1">
+                                <BackIcon
+                                    className="inline mr-1 mb-[2.25px] w-5 h-5 fill-black dark:fill-white"
+                                />
 
-                <DashboardBottomBar />
+                                Back to overview
+                            </MiscButton>
+                        </a>
+                        : null
+                    }
+                    <div className="flex items-center mb-8">
+                        <h1 className="text-4xl font-bold grow">
+                            {headerTitle}
+                        </h1>
+
+                        {headerButton}
+                    </div>
+
+                    {children}
+                </div>
             </div>
         </div>
     )
