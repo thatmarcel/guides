f2a4caf28209aaa862d50617b9c56eb15b29f8eb
thatmarcel <contact@thatmarcel.com>
2024-06-17 00:01:18 +0200

    Redirect "/" to "/dash"

--- next.config.js
+++ next.config.js
@@ -3,6 +3,16 @@ const createNextIntlPlugin = require("next-intl/plugin");
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async redirects() {
+        return [
+            {
+                source: "/",
+                destination: "/dash",
+                permanent: false
+            },
+        ]
+    }
+};
 
 module.exports = withNextIntl(nextConfig);
