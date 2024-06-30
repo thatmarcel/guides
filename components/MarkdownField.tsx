bced5ba2619419ea71c85703f7c3c46c4ced198d
thatmarcel <contact@thatmarcel.com>
2024-06-30 13:38:46 +0200

    Add checkbox to disable live markdown preview

--- components/MarkdownField.tsx
+++ components/MarkdownField.tsx
@@ -7,7 +7,7 @@ import {gfm} from "@milkdown/preset-gfm";
 import {history} from "@milkdown/plugin-history";
 import {clipboard} from "@milkdown/plugin-clipboard";
 import {listener, listenerCtx} from "@milkdown/plugin-listener";
-import {useEffect} from "react";
+import {useState} from "react";
 
 const MilkdownEditor = ({
     startValue,
@@ -49,26 +49,49 @@ export default function MarkdownField({
     className?: string,
     onMarkdownUpdated: (markdown: string) => void
 }) {
+    const [isMarkdownPreviewActive, setMarkdownPreviewActive] = useState(true);
+
     return (
         <div className={className}>
-            <p
-                className="text-lg font-medium pb-1"
-            >
-                {title} (supports <a
-                    className="underline underline-offset-2"
-                    href="https://www.markdownguide.org/cheat-sheet/#basic-syntax"
-                    target="_blank"
+            <div className="flex items-center">
+                <p
+                    className="text-lg font-medium pb-1"
                 >
-                    Markdown
-                </a>)
-            </p>
+                    {title} (supports <a
+                        className="underline underline-offset-2"
+                        href="https://www.markdownguide.org/cheat-sheet/#basic-syntax"
+                        target="_blank"
+                    >
+                        Markdown
+                    </a>)
+                </p>
 
-            <MilkdownProvider>
-                <MilkdownEditor
-                    startValue={startValue}
-                    onMarkdownUpdated={onMarkdownUpdated}
+                <div className="grow" />
+
+                <input
+                    type="checkbox"
+                    id="md-raw-checkbox"
+                    checked={isMarkdownPreviewActive}
+                    onChange={ e => setMarkdownPreviewActive(e.target.checked) }
                 />
-            </MilkdownProvider>
+                <label htmlFor="md-raw-checkbox" className="ml-2 select-none">
+                    Preview Markdown
+                </label>
+            </div>
+
+            {isMarkdownPreviewActive
+                ? <MilkdownProvider>
+                    <MilkdownEditor
+                        startValue={startValue}
+                        onMarkdownUpdated={onMarkdownUpdated}
+                    />
+                </MilkdownProvider>
+                : <textarea
+                    className="editor resize-none -mb-[0.4rem]"
+                    value={startValue}
+                    onChange={ e => onMarkdownUpdated(e.target.value) }
+                />
+            }
         </div>
     )
 }
